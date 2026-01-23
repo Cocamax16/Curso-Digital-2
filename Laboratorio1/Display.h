25299b6f74cf563ac1e727805005ac9b63756451
Cocamax16 <124897153+Cocamax16@users.noreply.github.com>
2026-01-22 20:11:56 -0600

    Create Display.h

---
+++ Laboratorio1/Display.h
@@ -0,0 +1,19 @@
+/*
+ * Display.h
+ *
+ * Created: 1/15/2026 7:25:38 PM
+ *  Author: Ususario
+ */ 
+
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+#include <avr/io.h>
+
+/*Se hace una sub definicio de la funcion o eso recuerdo yo*/
+void LedsDisplay(uint8_t Numero);
+
+
+
+#endif /* DISPLAY_H_ */
